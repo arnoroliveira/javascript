4c55e39372923d41e5dbad5471611bebf6ec850b
arnoroliveira <p.oliviras@gmail.com>
2024-11-04 12:25:47 -0300

    Add estudo JS

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+// const nome = 'Pablo'
+// let aprovado = true;
+// let nota = 8;
+
+// if(nota < 8) {
+//     aprovado = false;
+// }
+
+
+
+
+
+
+// const idade = 17;
+
+// if (idade > 18) {
+    
+//     console.log("Maior que 18");
+// } else {
+    
+//     console.log("Menor que 18");
+// }
